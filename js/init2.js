39e4bc3594e76692850140bb1bcaa5f5e1de4655
Alexis Ordenes <giraalexis@outlook.com>
2020-12-05 17:19:16 -0300

    Añadir Manifest
    Corregir  datos : N°pokedex, tipo pokemon
    Añadir icons
    Corregir visualizacion html

--- js/init2.js
+++ js/init2.js
@@ -7,24 +7,38 @@ window.addEventListener('DOMContentLoaded', async()=>{
     window.mostrar(pokemones);
 })
 
+//Mostrar los datos
 window.mostrar = async(pokemones)=>{
     const molde = document.querySelector(".molde");
     const contenedor = document.querySelector(".contenedor");
     for(let i=0; i< pokemones.length; i++){
         let p = pokemones[i];
         let copia = molde.cloneNode(true);
-        copia.querySelector(".pokemon-numero").innertText = p.entry_number;
+        copia.querySelector(".pokemon-numero").innerText = p.entry_number;
         copia.querySelector(".pokemon-nombre").innerText = p.pokemon_species.name.capitalize();
-        let imagen = await window.imagenPokemon(p.entry_number);
-        copia.querySelector(".pokemon-imagen").src = imagen;
+        let datos = await window.datosPokemon(p.entry_number); //buscar datos del pokemon(imagen,tipo)
+        copia.querySelector(".pokemon-imagen").src = datos[0]; //la imagen
+        copia.querySelector(".pokemon-tipo").innerText = datos[1]; //el tipo 1
+        copia.querySelector(".pokemon-tipo2").innerText = datos[2]; //el tipo 2
         contenedor.appendChild(copia);
     }
 }
 
-window.imagenPokemon = async (id)=>{
-    let respuesta = await axios.get("https://pokeapi.co/api/v2/pokemon/"+id+"/");
-    let imagen = respuesta.data.sprites.front_default;
-    return imagen;
+//Pedir imagen y tipo de pokemon
+window.datosPokemon = async (id)=>{
+    let respuesta = await axios.get("https://pokeapi.co/api/v2/pokemon/"+id+"/"); //se trae los datos del pokemon
+    let imagen = respuesta.data.sprites.front_default; //se trae la imagen por defecto
+    let tipo = respuesta.data.types[0].type.name.capitalize(); //se trae el primer tipo 
+    try{
+        let tipo2 = respuesta.data.types[1].type.name.capitalize(); //se trae el segundo tipo
+        let datos = [imagen,tipo,tipo2];
+        return datos;
+    }catch(e){
+        let tipo2 = "";
+        let datos = [imagen,tipo,tipo2];
+        return datos;
+    }
+    
 }
 
 //Capitalizar texto
